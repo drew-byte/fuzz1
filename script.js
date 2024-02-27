5c55b3cacdebed313e2e4a32d372b29d51579103
drew-byte <andrew.cunanan@neu.edu.ph>
2024-02-28 00:04:52 +0800

    init

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.getElementById('textForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var userInput = document.getElementById('textInput').value;
-    document.getElementById('output').innerHTML = "<strong>Entered Text:</strong><br>" + userInput;
-  });
-  
+  event.preventDefault();
+  var userInput = document.getElementById('textInput').value;
+
+  // Regular expression to check for specific HTML tags
+  var htmlTagRegex = /<(h[1-6]|p)>[^<>]*<\/(h[1-6]|p)>/i;
+
+  // Check if the user input contains any of the specified HTML tags
+  if (htmlTagRegex.test(userInput)) {
+    document.getElementById('output').innerText = "PWNEU{St0r3d-cr0xx-s!t3}";
+  } else {
+    // If not, display the entered text
+    document.getElementById('output').innerText = "Entered Text:\n" + userInput;
+  }
+});
